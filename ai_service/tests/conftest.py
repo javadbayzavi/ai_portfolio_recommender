2682b9050b41146642b95b65c56eb54e1ea77d55
Javad Bayzavi <javadbayzavi@gmail.com>
2025-11-09 15:31:12 +0100

    Project skeleton was introduced

---
+++ ai_service/tests/conftest.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+import sys
+
+
+project_root = Path(__file__).resolve().parent.parent
+sys.path.insert(0, str(project_root))
+
---
+++ backend/tests/main.test.ts
@@ -0,0 +1,6 @@
+
+describe("Should test first skeleton", () => {
+    it("Should say hello to world", async () => {
+
+    })
+});
