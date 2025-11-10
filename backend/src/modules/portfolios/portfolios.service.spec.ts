afc4cf8067477dde3325e205f9a79d5357ac802b
Javad Bayzavi <javadbayzavi@gmail.com>
2025-11-10 22:03:34 +0100

    Portfolio crud unittest were deployed

--- backend/src/modules/portfolios/portfolios.service.spec.ts
+++ backend/src/modules/portfolios/portfolios.service.spec.ts
@@ -1,18 +1,88 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { PortfoliosService } from "./portfolios.service";
+import { PrismaService } from "../../prisma/prisma.service";
+
+const mockPrisma = {
+  portfolio : {
+     findMany: jest.fn(),
+     findFirst: jest.fn(),
+     create: jest.fn(),
+     update: jest.fn(),
+     delete: jest.fn()
+  }
+}
 
 describe("PortfoliosService", () => {
   let service: PortfoliosService;
-
+  let prisma: PrismaService;
+  const mockedPortfolio = {
+    id : "1",
+    name : "Sample portfolio",
+    user_id : "1",
+    created_at : new Date(),
+    updated_at : new Date()
+  }
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PortfoliosService],
+      providers: [
+        PortfoliosService,
+        { provide: PrismaService, useValue:mockPrisma}
+      ],
     }).compile();
 
     service = module.get<PortfoliosService>(PortfoliosService);
+    prisma = module.get<PrismaService>(PrismaService);
   });
 
   it("should be defined", () => {
     expect(service).toBeDefined();
   });
+
+  it("should be defined", () => {
+    expect(prisma).toBeDefined()
+  })
+
+  it("should return all portfolios", async () => {
+    mockPrisma.portfolio.findMany.mockResolvedValue([mockedPortfolio])
+    let result_portfolio = await service.getPortfolios()
+    expect(result_portfolio).toEqual([mockedPortfolio])
+  })
+
+  it("should return a portfolio", async () => {
+    mockPrisma.portfolio.findFirst.mockResolvedValue(mockedPortfolio)
+    let result_portfolio = await service.getPortfolio("1")
+    expect(result_portfolio).toEqual(mockedPortfolio)
+  })
+
+  it("should create a portfolio", async () => {
+    let portfolio = {
+      name : "Sample portfolio",
+      user_id : "1",
+      created_at : new Date(),
+      updated_at : new Date()
+    }
+    mockPrisma.portfolio.create.mockResolvedValue(portfolio)
+    let result_portfolio = await service.createPortfolio(portfolio)
+    expect(result_portfolio).toEqual(portfolio)
+  })
+
+  it("should update a portfolio", async () => {
+    mockedPortfolio.name = "Updated portfolio"
+    mockPrisma.portfolio.update.mockResolvedValue(mockedPortfolio)
+    let result_portfolio = await service.updatePortfolio(mockedPortfolio)
+    expect(result_portfolio).toEqual(mockedPortfolio)
+  })
+
+  it("should delete a portfolio", async () => {
+    mockPrisma.portfolio.delete.mockResolvedValue({id : "1"})
+    let result = await service.deletePortfolio("1")
+    expect(result).toEqual({id : "1"})
+  })
+
+  it("should return user portfolios", async () => {
+    mockPrisma.portfolio.findMany.mockResolvedValue([mockedPortfolio])
+    let result_portfolio = await service.getUserPortfolios("1")
+    expect(result_portfolio).toEqual([mockedPortfolio])
+  })
+
 });
--- backend/src/modules/portfolios/portfolios.service.ts
+++ backend/src/modules/portfolios/portfolios.service.ts
@@ -1,17 +1,50 @@
 import { Injectable } from "@nestjs/common";
-import { PrismaService } from "prisma/prisma.service";
+import { PrismaService } from "../../prisma/prisma.service";
 
 @Injectable()
 export class PortfoliosService {
     constructor(private prisma: PrismaService){}
 
-    async getPortfolio(id: string){}
+    async getPortfolio(id: string){
+        return this.prisma.portfolio.findFirst({
+            where: {
+                id
+            }
+        })
+    }
 
-    async getPortfolios() {}
+    async getPortfolios() {
+        return this.prisma.portfolio.findMany()
+    }
 
-    async createPortfolio(portfolio: any) {}
+    async createPortfolio(portfolio: any) {
+        return this.prisma.portfolio.create({
+            data : portfolio
+        })
+    }
 
-    async updatePortfolio(portfolio: any) {}
+    async updatePortfolio(portfolio: any) {
+        return this.prisma.portfolio.update({
+            where: {
+                id: portfolio.id
+            },
+            data: portfolio
+        })
+    }
 
-    async deletePortfolio(id: string) {}
+    async deletePortfolio(id: string) {
+        return this.prisma.portfolio.delete({
+            where: {
+                id
+            }
+        })
+    }
+
+    async getUserPortfolios(userId: string){
+        return this.prisma.portfolio.findMany({
+            where: {
+                user_id: userId
+            }
+        })
+    }
 }
