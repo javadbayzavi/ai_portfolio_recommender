4c7ad818014e5c6fb1c42cd4b44af584fc7e7af3
Javad Bayzavi <javadbayzavi@gmail.com>
2025-11-10 16:48:22 +0100

    Add portfolio service skeleton

--- backend/src/modules/portfolios/portfolios.service.ts
+++ backend/src/modules/portfolios/portfolios.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "prisma/prisma.service";
 
 @Injectable()
-export class PortfoliosService {}
+export class PortfoliosService {
+    constructor(private prisma: PrismaService){}
+
+    async getPortfolio(id: string){}
+
+    async getPortfolios() {}
+
+    async createPortfolio(portfolio: any) {}
+
+    async updatePortfolio(portfolio: any) {}
+
+    async deletePortfolio(id: string) {}
+}
