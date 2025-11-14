2d2af618b83a671a63bd5711e7cbca7dd5318907
Javad Bayzavi <javadbayzavi@gmail.com>
2025-11-14 12:47:19 +0100

    Add more redis specific features

--- ai_service/src/services/cache_service.py
+++ ai_service/src/services/cache_service.py
@@ -1,33 +1,42 @@
 from redis import Redis
+import logging
+import json
 
+        
 class CacheTemplate:
-    response : str
-    requester: str
-    subject: str
+    def __init__(self):
+        self.id: str
+        self.response : str
+        self.requester: str
+        self.subject: str
 
+logger = logging.getLogger("CacheService")
 
 
 class CacheService:
     def __init__(self):
-        self.redis = Redis(host='localhost', port=6379, decode_responses=True)
+        self.redis = Redis(host='localhost', db=0, port=6379, decode_responses=True)
         pass
 
-    def get(self, key):
+    def get(self, key) -> CacheTemplate:
         self.redis.get_cache(key)
+        raw_json = self.redis.json().get(key, ".")
+        return json.loads(raw_json)
 
-    def set(self, key, value):
+    def set(self, key, value: CacheTemplate):
         self.redis.set(key, value)
-        self.redis.json().set(key, ".", value)
+        self.redis.json().set(key, ".", json.dumps(value))
     
     def delete(self, key):
-        pass
+        self.redis.json().delete(key, ".")
+        self.redis.delete(key)
 
     def clear(self):
-        pass
+        self.redis.flushdb()
 
     def initiate(self):
         if self.redis is None:
-            self.redis = Redis(host='localhost', port=6379, decode_responses=True)
+            self.redis = Redis(host='localhost', db=0, port=6379, decode_responses=True)
 
 cache_service = CacheService()
 cache_service.initiate()
