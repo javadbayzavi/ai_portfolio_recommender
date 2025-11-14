010ab4829e5a2eab1234539b7f80d78cb9c07275
Javad Bayzavi <javadbayzavi@gmail.com>
2025-11-14 21:34:22 +0100

    add pydantic to project, now it have more compliance with api documentation

--- ai_service/src/ai_api/models/request_model.py
+++ ai_service/src/ai_api/models/request_model.py
@@ -1,6 +1,7 @@
 from ai_api.process_command import AICommand
+from pydantic import BaseModel
 
-class RequestModel():
+class RequestModel(BaseModel):
     def __init__(self, command: AICommand, params: dict[str, str]):
         self.command = command
         self.params = params
