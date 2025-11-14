0844c43376c23dfd2929b4b08b9a06a92d4d7a4e
Javad Bayzavi <javadbayzavi@gmail.com>
2025-11-14 12:48:46 +0100

    introduce process requester on top of observer pattern (command pattern)

---
+++ ai_service/src/ai_api/process_requester.py
@@ -0,0 +1,22 @@
+from ai_api.models import RequestModel
+from ai_api.models import ResponseModel
+
+
+process_handlers : dict[str, callable] = {}
+
+def register_process_handler(command: str, handler: callable):
+    process_handlers[command] = handler
+
+def get_process_handler(command: str) -> callable:
+    return process_handlers.get(command, None)
+
+
+async def process_request(request: RequestModel) -> ResponseModel:
+    handler = get_process_handler(request.command)
+    result : dict[str, str] = {}
+    
+    if handler is None:
+        result = handler(request.params)
+
+    return ResponseModel(result)
+
