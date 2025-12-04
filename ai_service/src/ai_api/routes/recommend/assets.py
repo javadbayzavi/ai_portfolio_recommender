c078a6b205e63d7fe302f257d817b98f8913b5de
Javad Bayzavi <javadbayzavi@gmail.com>
2025-12-04 13:50:25 +0100

    Now replace functiona-based router with class-based

--- ai_service/src/ai_api/routes/recommend/assets.py
+++ ai_service/src/ai_api/routes/recommend/assets.py
@@ -6,25 +6,17 @@ from services.recommend.assets import AssetRecommendationService
 class AssetsModel(RequestModel):
     assets: list[str]
 
-# router = APIRouter(prefix="/recommend/assets")
-
-# @router.post("/")
-# async def get_recommend(request: AssetsModel) -> ResponseModel:
-#     pass
-
-# @router.get("/{asset}")
-# async def get_recommend(asset: str) -> ResponseModel:
-#     result = AssetRecommendationService().getRecommend(asset=asset)
-#     return ResponseModel(response=result)
-
 class AssetRecommendationRouter:
     def __init__(self, asset_recommender_service: AssetRecommendationService = None):
         self.service = asset_recommender_service if asset_recommender_service is not None else AssetRecommendationService()
         self.router = APIRouter(prefix="/recommend/assets")
+        self.init_routes()
+    
+    def init_routes(self):
+        self.router.post("/")(self.get_recommend)
+        self.router.get("/{asset}")(self.get_recommend)
 
     def getRoute(self):
-        self.router.add_route("/", self.get_recommend, methods=["POST"])
-        self.router.add_route("/{asset}", self.get_recommend, methods=["GET"] )
         return self.router
 
 
